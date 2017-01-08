d4775862c0311b63fc67adfadba057c86075979f
pravdomil <pravdomil.toman@gmail.com>
2017-01-08 21:24:04 +0100

    reorder

--- index.js
+++ index.js
@@ -134,16 +134,17 @@ class Connection {
     debug('got %s', FCGI.GetMsgType(record.type))
     
     switch(record.type) {
-      case FCGI.MSG.END_REQUEST:
-        this.res.end()
+      case FCGI.MSG.STDERR:
         break
       
       case FCGI.MSG.STDOUT:
         this.stdout(record.content)
         break
       
-      case FCGI.MSG.STDERR:
+      case FCGI.MSG.END_REQUEST:
+        this.res.end()
         break
+      
     }
   }
   
