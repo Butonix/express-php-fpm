b8879ef7f533d9e8c5d29e2af990ef3eeade7289
pravdomil <pravdomil.toman@gmail.com>
2017-01-08 19:00:06 +0100

    DONT_KEEP_CONN

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ class Connection {
     this.socket.on('data', this.data.bind(this))
     
     // send req
-    this.send(FCGI.MSG.BEGIN_REQUEST, FCGI.BeginRequestBody(FCGI.ROLE.RESPONDER, 0))
+    this.send(FCGI.MSG.BEGIN_REQUEST, FCGI.BeginRequestBody(FCGI.ROLE.RESPONDER, FCGI.DONT_KEEP_CONN))
     this.send(FCGI.MSG.PARAMS, FCGI.NameValuePair(env))
     this.send(FCGI.MSG.PARAMS, Buffer.alloc(0))
     req.on('data', this.reqData.bind(this))
